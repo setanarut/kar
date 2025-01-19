277a364794fbd3e8e259ea511b6e323651c5c3e1
Barış <duurkak@yandex.com>
2025-01-19 22:38:17 +0300

    static collision check func

--- arc/components.go
+++ arc/components.go
@@ -1,5 +1,7 @@
 package arc
 
+import "math"
+
 type ItemID struct {
 	ID uint16
 }
@@ -47,33 +49,72 @@ func (r *Rect) CheckCollision(r2 *Rect, velX, velY float64) CollisionInfo {
 		Normal: [2]int{0, 0},
 	}
 
-	// İlk pozisyonları belirle
-	nextX := r.X + velX
-	nextY := r.Y + velY
-
-	// Önce X ekseninde hareket et ve kontrol et
-	if r2.Overlaps2(nextX, r.Y, r.W, r.H) {
+	// Önce statik çarpışma kontrolü
+	if r2.Overlaps2(r.X, r.Y, r.W, r.H) {
 		info.Collided = true
-		if velX > 0 {
-			info.DeltaX = r2.X - (nextX + r.W)
-			info.Normal[0] = -1
-		} else if velX < 0 {
-			info.DeltaX = (r2.X + r2.W) - nextX
-			info.Normal[0] = 1
+
+		// Statik çarpışmada push direction'ı belirle
+		centerX1 := r.X + r.W/2
+		centerX2 := r2.X + r2.W/2
+		centerY1 := r.Y + r.H/2
+		centerY2 := r2.Y + r2.H/2
+
+		diffX := centerX1 - centerX2
+		diffY := centerY1 - centerY2
+
+		// Hangi eksende daha az örtüşme var ise o yönde it
+		overlapX := r.W/2 + r2.W/2 - math.Abs(diffX)
+		overlapY := r.H/2 + r2.H/2 - math.Abs(diffY)
+
+		if overlapX < overlapY {
+			if diffX > 0 {
+				info.Normal[0] = 1
+				info.DeltaX = overlapX
+			} else {
+				info.Normal[0] = -1
+				info.DeltaX = -overlapX
+			}
+		} else {
+			if diffY > 0 {
+				info.Normal[1] = 1
+				info.DeltaY = overlapY
+			} else {
+				info.Normal[1] = -1
+				info.DeltaY = -overlapY
+			}
 		}
-		// X çarpışması varsa Y hareketini koru ama çarpışmayı X olarak işaretle
+
 		return info
 	}
 
-	// Sonra Y ekseninde hareket et ve kontrol et
-	if r2.Overlaps2(nextX, nextY, r.W, r.H) {
-		info.Collided = true
-		if velY > 0 {
-			info.DeltaY = r2.Y - (nextY + r.H)
-			info.Normal[1] = -1
-		} else if velY < 0 {
-			info.DeltaY = (r2.Y + r2.H) - nextY
-			info.Normal[1] = 1
+	// Hareket varsa dinamik çarpışma kontrolü
+	if math.Abs(velX) > 0 || math.Abs(velY) > 0 {
+		nextX := r.X + velX
+		nextY := r.Y + velY
+
+		// Önce X ekseninde hareket et ve kontrol et
+		if r2.Overlaps2(nextX, r.Y, r.W, r.H) {
+			info.Collided = true
+			if velX > 0 {
+				info.DeltaX = r2.X - (nextX + r.W)
+				info.Normal[0] = -1
+			} else if velX < 0 {
+				info.DeltaX = (r2.X + r2.W) - nextX
+				info.Normal[0] = 1
+			}
+			return info
+		}
+
+		// Sonra Y ekseninde hareket et ve kontrol et
+		if r2.Overlaps2(nextX, nextY, r.W, r.H) {
+			info.Collided = true
+			if velY > 0 {
+				info.DeltaY = r2.Y - (nextY + r.H)
+				info.Normal[1] = -1
+			} else if velY < 0 {
+				info.DeltaY = (r2.Y + r2.H) - nextY
+				info.Normal[1] = 1
+			}
 		}
 	}
 
